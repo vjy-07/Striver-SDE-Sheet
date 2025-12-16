848dc2dc542ea751fdb9e3940be77c3e98cbfa41
vjy-07 <gvijaycrl@gmail.com>
2025-12-16 23:29:57 +0530

    Merge Intervals

---
+++ 2. Arrays Part2/Merge Intervals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        vector<vector<int>> res;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<n;i++){
+            if(res.empty() || intervals[i][0]> res.back()[1]){
+                res.push_back(intervals[i]);
+            }
+            else{
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
+        }
+        return res;
+    }
+};
